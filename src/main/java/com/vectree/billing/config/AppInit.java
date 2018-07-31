179fcbd519cfada8a74913ea6244053935258f39
vakhnenzon <gmail@vakhnenko.com>
2018-07-31 18:00:47 +0300

    init spring mvc

---
+++ src/main/java/com/vectree/billing/config/AppInit.java
@@ -0,0 +1,28 @@
+package com.vectree.billing.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * class AppInit WebApplicationInitializer Spring MVC.
+ *
+ * @version 1.0
+ */
+public class AppInit implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("billing", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/java/com/vectree/billing/config/RootConfig.java
@@ -0,0 +1,14 @@
+package com.vectree.billing.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * class RootConfig Spring MVC.
+ *
+ * @version 1.0
+ */
+@Configuration
+@ComponentScan({"com.vectree.billing.*"})
+public class RootConfig {
+}
